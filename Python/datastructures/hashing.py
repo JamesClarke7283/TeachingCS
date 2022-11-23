f1a27a0b956dd8bfab3e08fe941420cd7b84c154
James Clarke <james@james-clarke.ynh.fr>
2022-11-23 14:06:17 +0000

    Added HashMap

---
+++ Python/datastructures/hashing.py
@@ -0,0 +1,32 @@
+class HashMap:
+    def __init__(self, size):
+        self.size = size
+        self._hash_lst = [None for i in range(self.size)]
+
+    def hash_code(self, key: int):
+        return key % self.size
+
+    def add(self, key):
+        self._hash_lst[self.hash_code(key)] = key
+
+    def remove(self, key):
+        self._hash_lst[self.hash_code(key)] = None
+
+    def get(self, key):
+        index = self.hash_code(key)
+        if index <= len(self._hash_lst):
+            if self._hash_lst[index] is not None:
+                return self._hash_lst[index]
+        return -1
+
+
+def main():
+    hm = HashMap(64)
+    hm.add(203)
+    hm.add(50)
+    hm.add(60)
+    print(hm.get(50))
+
+
+if __name__ == "__main__":
+    main()
