c26a74f57812ed9a2eeeed0ed7f227e8696ae24c
Anonymouse <anonymouse@james-clarke.ynh.fr>
2023-01-26 11:35:11 -0600

    made sub and mul fction

--- Python/cli/calc-cli.py
+++ Python/cli/calc-cli.py
@@ -14,13 +14,24 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Calculates math operations')
 
-parser.add_argument("--add", action="extend", nargs="+", type=float)
+op_lst = ["add", "sub", "mul", "div"]
+for op in op_lst:
+    parser.add_argument("--" + op, action="extend", nargs="+", type=float)
 
 args = parser.parse_args()
-answer = None
+answer = 0
 
 if args.add:
     answer = sum(args.add)
+if args.sub:
+    subtract_set = args.sub[1::]
+    answer = args.sub[0] - sum(subtract_set)
+if args.mul:
+    prev_num = 0
+    for index in range(0, len(args.mul), 2):
+        if index >= 1:
+            answer += prev_num * args.mul[index]
+        prev_num = args.mul[index]
 
 # Check if answer is whole number, if so, set answer to integer
 if answer % 1 == 0:
