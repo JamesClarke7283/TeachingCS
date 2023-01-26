9e599ea1e9280a6638432ea8a744d041eb4bf474
James Clarke <james@james-clarke.ynh.fr>
2023-01-26 16:55:12 +0000

    Added first argument parse in calculator

--- Python/cli/calc-cli.py
+++ Python/cli/calc-cli.py
@@ -1,3 +1,5 @@
+import argparse
+
 """ Calculator
  Add
  Sub
@@ -5,7 +7,23 @@
  Divide
 """
 # Command Examples
-# cal-cli.py 1 add 2
-# cal-cli.py 1 sub 2
-# cal-cli.py 1 multi 2
-# cal-cli.py 1 div 2
+# cal-cli.py --add 1 2
+# cal-cli.py --sub 1 2
+# cal-cli.py --mul 1 2
+# cal-cli.py --div 1 2
+
+parser = argparse.ArgumentParser(description='Calculates math operations')
+
+parser.add_argument("--add", action="extend", nargs="+", type=float)
+
+args = parser.parse_args()
+answer = None
+
+if args.add:
+    answer = sum(args.add)
+
+# Check if answer is whole number, if so, set answer to integer
+if answer % 1 == 0:
+    answer = int(answer)
+print(answer)
+print("DEBUG:",args)
