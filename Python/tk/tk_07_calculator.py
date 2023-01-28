3fa5d2939c881ed8c3ae1ac4e8e56cc1c0fc561d
James Clarke <james@james-clarke.ynh.fr>
2023-01-28 18:59:23 +0000

    Completed Calculator TK

--- Python/tk/tk_07_calculator.py
+++ Python/tk/tk_07_calculator.py
@@ -1,4 +1,13 @@
 from tkinter import *
+from enum import IntEnum
+
+
+class Operation(IntEnum):
+    ADD = 0
+    SUB = 1
+    MUL = 2
+    DIV = 3
+
 
 window = Tk()
 
@@ -13,48 +22,112 @@ result_lbl.pack(side="top")
 number_pad = Frame(calc_frame)
 number_pad.pack(side="bottom")
 
+accumulator = 0
+op = Operation
+
 def btn_0_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"0")
+
+
 def btn_1_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"1")
+
+
 def btn_2_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"2")
+
+
 def btn_3_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"3")
+
+
 def btn_4_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"4")
+
+
 def btn_5_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"5")
+
+
 def btn_6_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"6")
+
+
 def btn_7_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"7")
+
+
 def btn_8_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"8")
+
+
 def btn_9_clicked():
     current_text = result_lbl['text']
     result_lbl.config(text=current_text+"9")
 
+
 def btn_add_clicked():
-    pass
+    global accumulator
+    global op
+    accumulator = int(result_lbl['text'])
+    result_lbl.config(text="")
+    op = Operation.ADD
+
 
 def btn_sub_clicked():
-    pass
+    global accumulator
+    global op
+    accumulator = int(result_lbl['text'])
+    result_lbl.config(text="")
+    op = Operation.SUB
+
 
 def btn_mul_clicked():
-    pass
+    global accumulator
+    global op
+    accumulator = int(result_lbl['text'])
+    result_lbl.config(text="")
+    op = Operation.MUL
+
 
 def btn_div_clicked():
+    global accumulator
+    global op
+    accumulator = int(result_lbl['text'])
+    result_lbl.config(text="")
+    op = Operation.DIV
+
+
+def btn_clear_clicked():
+    result_lbl.config(text="")
     pass
+
+
+def btn_calc_clicked():
+    global op
+    global accumulator
+    second_accumulator = int(result_lbl['text'])
+    result = 0
+    match op:
+        case Operation.ADD:
+            result = accumulator + second_accumulator
+        case Operation.SUB:
+            result = accumulator - second_accumulator
+        case Operation.MUL:
+            result = accumulator * second_accumulator
+        case Operation.DIV:
+            result = accumulator / second_accumulator
+    result_lbl.config(text=str(result))
+
 
 btn_0 = Button(number_pad, text="0", command=btn_0_clicked)
 btn_0.grid(column=1, row=3)
@@ -98,6 +171,10 @@ btn_mul.grid(column=4, row=2)
 btn_div = Button(number_pad, text="/", command=btn_div_clicked)
 btn_div.grid(column=4, row=3)
 
+btn_clear = Button(number_pad, text="C", command=btn_clear_clicked)
+btn_clear.grid(column=2, row=3)
 
+btn_calc = Button(number_pad, text="=", command=btn_calc_clicked)
+btn_calc.grid(column=3, row=3)
 
 window.mainloop()
