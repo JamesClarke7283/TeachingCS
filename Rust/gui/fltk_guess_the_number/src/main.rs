99ecc92a1e49926440b0291997095c845fec6e7e
James Clarke <james@james-clarke.ynh.fr>
2023-01-29 13:46:03 +0000

    Completed guess the number program

--- Rust/gui/fltk_guess_the_number/src/main.rs
+++ Rust/gui/fltk_guess_the_number/src/main.rs
@@ -1,25 +1,28 @@
 extern crate fltk;
 
-use fltk::{app, button::*, input::*, window::*, prelude::*, text::*};
+use fltk::{app, button::*, input::*, prelude::*, text::*, window::*};
+
 fn main() {
     let app = app::App::default();
     let mut win = Window::new(100, 100, 400, 300, "Guess the Number Game");
-    let mut input = IntInput::new(120, 50, 150, 30, "Enter a number:");
-    let mut message = TextDisplay::new(120, 100, 150, 30, "");
+    let input = IntInput::new(120, 50, 150, 30, "Enter a number:");
+    let message = TextDisplay::new(120, 100, 150, 30, "");
     let mut submit = Button::new(180, 130, 80, 30, "Submit");
     let mut new_game = Button::new(120, 170, 150, 30, "New Game");
-
-    let mut secret_number = &rand::random::<u32>() % 20;
-
-    
-    new_game.set_callback(move |_| {
-        secret_number = rand::random::<u32>() % 100;
-        input.set_value("0");
-        message.set_label("");
-    });
-    
-    
-     
+    const MAX_NUMBER: u32 = 20;
+    let mut secret_number = *&mut rand::random::<u32>() % MAX_NUMBER;
+    {
+        let mut input = input.clone();
+        let mut message = message.clone();
+        new_game.set_callback(move |_| {
+            secret_number = rand::random::<u32>() % MAX_NUMBER;
+            input.set_value("");
+            message.set_label("");
+        });
+    }
+{
+    let input = input.clone();
+    let mut message = message.clone();
     submit.set_callback(move |_| {
         let guess = input.value().parse::<u32>().unwrap();
         if guess == secret_number {
@@ -30,8 +33,7 @@ fn main() {
             message.set_label("Too high. Try again.");
         }
     });
-    
-    
+}
     win.end();
     win.show();
     app.run().unwrap();
