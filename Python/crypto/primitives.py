c136a029139c2a95ae3e82192fc0e2a6a9437ca1
James Clarke <james@james-clarke.ynh.fr>
2023-02-15 19:20:51 +0000

    Added primitives module to crypto

---
+++ Python/crypto/primitives.py
@@ -0,0 +1,13 @@
+""" Encryption and decryption primitives.
+ord : gets a number from a character
+chr : gets a character from a number
+charset : string.ascii_letters + string.digits + string.punctuation + " "
+modulus : The size of a number
+
+Step 1: Convert the message to a list of numbers
+Step 2: Add the key to each number
+Step 3: Convert the numbers back to characters
+"""
+
+message = "Hello Anon"
+
