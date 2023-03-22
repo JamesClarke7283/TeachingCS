2ca8700a2b8808dd1186678673d848910daff500
James Clarke <james@james-clarke.ynh.fr>
2023-03-22 18:39:06 +0000

    Added sorter cli program

---
+++ Python/cli/sorter.py
@@ -0,0 +1,12 @@
+import sys
+
+numbers = sys.argv[1::]
+
+# Convert the list of strings to a list of integers
+new_numbers = []
+for number in numbers:
+    new_numbers.append(int(number))
+
+# Sort the list of integers
+
+    
