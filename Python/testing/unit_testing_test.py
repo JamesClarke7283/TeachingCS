8f0bca51c550b5769d3b604e182da191634120e4
James Clarke <james@james-clarke.ynh.fr>
2023-03-26 17:40:58 +0100

    Moved zebracase outside class on unit_testing tutorial module

--- Python/testing/unit_testing_test.py
+++ Python/testing/unit_testing_test.py
@@ -1,22 +1,23 @@
 import unittest
+"""Unit testing is where you testware in components called units, you test each unit using a testing framework.
+"""
 
-class StringMethods:
-    @staticmethod
-    def zebracase(string):
-        new_str = list(string.lower())
-        for index in range(len(string)):
-            if index % 2 == 0:
-                new_str[index] = string[index].upper()
-        new_str = "".join(new_str)
-        return new_str
+
+def zebracase(string):
+    new_str = list(string.lower())
+    for index in range(len(string)):
+        if index % 2 == 0:
+            new_str[index] = string[index].upper()
+    new_str = "".join(new_str)
+    return new_str
 
 
 class TestStringMethods(unittest.TestCase):
     def test_normal(self):
-        self.assertEqual("TeSt", StringMethods.zebracase("test"))
-    
+        self.assertEqual("TeSt", zebracase("test"))
+
     def test_fuzzy(self):
-        self.assertEqual("TeStCaSe", StringMethods.zebracase("tESTcASE"))
-    
+        self.assertEqual("TeStCaSe", zebracase("tESTcASE"))
+
     def test_extreme(self):
-        self.assertEqual("PnEuMoNoUlTrAmIcRoScOpIcSiLiCoVoLcAnOcOnIoSiS", StringMethods.zebracase("pneumonoultramicroscopicsilicovolcanoconiosis"))
+        self.assertEqual("PnEuMoNoUlTrAmIcRoScOpIcSiLiCoVoLcAnOcOnIoSiS", zebracase("pneumonoultramicroscopicsilicovolcanoconiosis"))
