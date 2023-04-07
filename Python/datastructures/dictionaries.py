7a260751dca24cac6d7348f41d7fe968ad84ecf4
James Clarke <james@james-clarke.ynh.fr>
2023-04-07 18:43:52 +0100

    Added dictionary section to datastructures

---
+++ Python/datastructures/dictionaries.py
@@ -0,0 +1,11 @@
+"""Dictionaries are a collection of key-value pairs. They are unordered and
+mutable. They are indexed by keys, which can be any immutable type; strings and
+numbers can always be keys.
+"""
+
+# Create a dictionary
+# The keys are strings and the values are numbers
+my_dict = {"one": 1, "two": 2, "three": 3}
+
+# Access a value by its key
+print(my_dict["one"])  # 1
